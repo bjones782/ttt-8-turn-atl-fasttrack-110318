4054e404a195e6e340bb18babc638d4bdfad289a
Brooke Jones <bjones782@gmail.com>
2019-02-17 18:15:58 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,11 +38,9 @@ def turn(board)
   user_input = gets.chomp
   index = input_to_index(user_input)
   if valid_move?(board, index)
-    player_token = current_player(board)
     move(board, index, player_token)
     display_board(board)
   else
     turn(board)
   end
-end
 end
