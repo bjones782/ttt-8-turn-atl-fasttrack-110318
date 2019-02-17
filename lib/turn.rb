c879beea86dc6615b6c2eaa9453aed2018cfeff0
Brooke Jones <bjones782@gmail.com>
2019-02-17 18:04:39 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -25,16 +25,6 @@ def position_taken?(board, index)
   end
 end
 
-def current_player(board)
-  #if the turn count is an even number, that means O just went, so the next/current player is X
-  num_turns = turn_count(board)
-  if num_turns % 2 == 0
-    player = "X"
-  else
-    player = "O"
-  end
-  return player
-end
 
 def turn(board)
   puts "Please choose a number 1-9:"
